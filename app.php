5e805c56dcd787f31a55b677606d3b54f523a974
Pablo Godel <pgodel@gmail.com>
2012-10-17 08:57:29 -0400

    added silex app

---
+++ app.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once __DIR__.'/silex.phar';
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+$app = new Silex\Application();
+
+$app['debug'] = true;
+
+$app->post('/', function(Request $request) {
+    file_put_contents('mail', $request->getContent());
+    
+    return new Response();
+});
+
+$app->error(function (\Exception $e, $code) {
+    switch ($code) {
+        case 404:
+            $message = 'The requested page could not be found.';
+            break;
+        default:
+            $message = 'We are sorry, but something went terribly wrong.';
+    }
+
+    return new Response($message, $code);
+});
+
+$app->run();
